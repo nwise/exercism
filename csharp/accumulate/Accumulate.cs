605dd8a69cb9866606fa5ca4786d4266599c85d1
Nathan Wise <nathanwise@gmail.com>
2017-12-10 15:57:30 -0500

    Adding Accumulate

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -5,8 +5,9 @@ public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        foreach(T x in collection){
-            yield return func(x);
+        foreach (T item in collection)
+        {
+            yield return func.Invoke(item);
         }
     }
 }
