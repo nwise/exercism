26cacde88fe0c45d9338e539ce02aa0fdccd55c0
Nathan Wise <nathanwise@gmail.com>
2017-12-10 19:24:31 -0500

    Grade School

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -3,18 +3,39 @@ using System.Collections.Generic;
 
 public class School
 {
+    public Dictionary<int, List<string>> list = new Dictionary<int, List<string>>();
+
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if(!list.ContainsKey(grade)){
+            InitGrade(grade);
+        } 
+        list[grade].Add(student);
+        list[grade].Sort();
     }
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<int> grades = new List<int>(list.Keys);
+        List<string> students = new List<string>();
+        grades.Sort();
+        foreach(int grade in grades){
+            foreach(string student in list[grade]){
+                students.Add(student);
+            }
+        }
+        return students;
     }
 
     public IEnumerable<string> Grade(int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if(!list.ContainsKey(grade)){
+            InitGrade(grade);
+        }
+        return list[grade];
+    }
+
+    private void InitGrade(int grade){
+        list.Add(grade, new List<string>()); 
     }
 }
